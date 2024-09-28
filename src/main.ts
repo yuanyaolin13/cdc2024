d5d313086a943ef22f0332deeb089b64c64f7caa
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-09-28 16:23:36 -0400

    website testing pt.2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from './App.vue';
+import {createApp} from 'vue';
+import './style.css';
 
-createApp(App).mount('#app')
+// Initialization of app object
+const app = createApp(App);
+
+// To mount the object upon
+app.mount('#app');
