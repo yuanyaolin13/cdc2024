a94c1283489158ee2c5e8d21a9b378064a893324
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-09-28 17:52:46 -0400

    website testing pt.5

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
     // For example <img src="/images/logo.png"> will not work without the code below
   }),],
-  base: '/cdc2024/',
+  base: './',
   resolve: {
     alias: [
 
