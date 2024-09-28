fd390945e1e0b4621eb8dc6ff5e9ba7f3dc777ef
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-09-28 16:36:50 -0400

    website testing pt.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+    // For example <img src="/images/logo.png"> will not work without the code below
+  }),],
+  base: '/cdc2024/',
+  resolve: {
+    alias: [
+
+    ]
+  },
 })
